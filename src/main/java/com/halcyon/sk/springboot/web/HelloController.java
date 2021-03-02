1c040bc429033e630b0289c06860f795fffd5c2c
halcyon-sh <symanitos2@gmail.com>
2021-03-03 00:20:59 +0900

    Test Localhost Code

---
+++ src/main/java/com/halcyon/sk/springboot/web/HelloController.java
@@ -0,0 +1,11 @@
+package com.halcyon.sk.springboot.web;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+}
