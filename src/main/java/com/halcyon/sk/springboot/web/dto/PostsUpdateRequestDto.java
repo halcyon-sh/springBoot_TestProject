920b99d95b3448b88ff6d7fdb48afdbc736b0136
halcyon-sh <symanitos2@gmail.com>
2021-03-10 21:46:39 +0900

    Part3. Complite Code(1)

---
+++ src/main/java/com/halcyon/sk/springboot/web/dto/PostsUpdateRequestDto.java
@@ -0,0 +1,19 @@
+package com.halcyon.sk.springboot.web.dto;
+
+import com.halcyon.sk.springboot.domain.posts.Posts;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class PostsUpdateRequestDto {
+    private String title;
+    private String content;
+
+    @Builder
+    public PostsUpdateRequestDto(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+}
